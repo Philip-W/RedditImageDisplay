882f132e9029e97782f16df102e6f7541d968a42
Philip Woods <sc14pw@leeds.ac.uk>
2017-05-28 15:04:46 +0100

    Used JSoup to collect page html, then parse 'data-url' attributes from reddit posts

--- app/src/main/java/base/earthgrabber/network/ImageDownloader.java
+++ app/src/main/java/base/earthgrabber/network/ImageDownloader.java
@@ -4,6 +4,15 @@ import android.media.Image;
 import android.os.AsyncTask;
 import android.util.Log;
 
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+import org.w3c.dom.Document;
+import org.xml.sax.InputSource;
+import org.xml.sax.SAXException;
+import org.xml.sax.XMLReader;
+import org.xml.sax.helpers.XMLReaderFactory;
+
 import java.io.BufferedInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
@@ -11,6 +20,12 @@ import java.io.InputStream;
 import java.net.MalformedURLException;
 import java.net.URL;
 
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+
+
+import org.jsoup.*;
 /**
  * Created by Philip on 24/05/2017.
  */
@@ -18,6 +33,8 @@ import java.net.URL;
 public class ImageDownloader  extends AsyncTask{
     @Override
     protected Object doInBackground(Object[] objects) {
+
+        /*
         Image image = null;
         byte[] data = {};
         try {
@@ -46,5 +63,46 @@ public class ImageDownloader  extends AsyncTask{
         }
 
         return data;
+    }
+*/
+
+       // try {
+            //URL url = new URL("https://www.reddit.com/r/EarthPorn/top/?sort=top&t=day");
+            //DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+            //DocumentBuilder db = dbf.newDocumentBuilder();
+            //Document doc = db.parse(url.openStream());
+            Log.d("URL", "starting...");
+        org.jsoup.nodes.Document doc = null;
+        try {
+            doc = Jsoup.connect("https://www.reddit.com/r/EarthPorn/top/?sort=top&t=day").get();
+        } catch (IOException e) {
+                e.printStackTrace();
+        }
+        Log.d("URL", "got doc");
+           // Elements images = doc.select("div.imageInlineCenter img[src]");
+        //Elements images = doc.getElementsByTag("");
+        Elements images = doc.getElementsByAttribute("data-url");
+
+                //doc.select("div.node div.content img");
+
+        for (Element e : images){
+            Log.d("IMG", e.attr("data-url"));
+
+            //Log.d("IMG", e.attr("src");
+        }
+
+
+/*
+        } catch (SAXException e) {
+            e.printStackTrace();
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (ParserConfigurationException e) {
+            e.printStackTrace();
+        }
+*/
+        return true;
     }
 }
