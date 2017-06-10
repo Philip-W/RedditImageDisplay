e932207e9cdc3e85f65793c39c43ff38ac54991c
Philip Woods <sc14pw@leeds.ac.uk>
2017-06-10 20:33:42 +0100

    Added comments to WidgetProvider

--- app/src/main/java/base/earthgrabber/WidgetProvider.java
+++ app/src/main/java/base/earthgrabber/WidgetProvider.java
@@ -40,32 +40,35 @@ public class WidgetProvider extends AppWidgetProvider {
         // Widget size currently set to 250 x 110dp
 
         for (int i = 0; i < count; i++) {
+            // Get the file path and load the bitmap
             String filePath = context.getFilesDir().listFiles()[2].getPath();
-
-
             Bitmap bitmap = getScaledBitmapFromFile(filePath, displayWidth, displayHeight);
 
 
+            // Set the bitmap to the image view of the widget
             int widgetId = appWidgetIds[i];
-
             RemoteViews remoteViews = new RemoteViews(context.getPackageName(),
                     R.layout.widget_main);
-
             remoteViews.setImageViewBitmap(R.id.imageView, bitmap);
 
+            //Finally notify the manager to update the widget
             appWidgetManager.updateAppWidget(widgetId, remoteViews);
             //imageCycle = (imageCycle + 1) % context.getFilesDir().list().length;
         }
     }
 
     public static Bitmap getScaledBitmapFromFile(String filePath, int reqWidth, int reqHeight){
+
         BitmapFactory.Options options = new BitmapFactory.Options();
         options.inJustDecodeBounds = true;
 
+        // Load the bitmap (in decode bounds)
         BitmapFactory.decodeFile(filePath, options);
 
+        // calculate the scaling size
         options.inSampleSize = calculateInSampleSize(options, reqWidth, reqHeight);
 
+        // Load the bitmap fully
         options.inJustDecodeBounds = false;
         Bitmap bMap = BitmapFactory.decodeFile(filePath, options);
 
@@ -74,18 +77,19 @@ public class WidgetProvider extends AppWidgetProvider {
 
     public static int calculateInSampleSize(
             BitmapFactory.Options options, int reqWidth, int reqHeight) {
-        // Raw height and width of image
+        // Get actual image size
         final int height = options.outHeight;
         final int width = options.outWidth;
         int inSampleSize = 1;
+
 
         if (height > reqHeight || width > reqWidth) {
 
             final int halfHeight = height / 2;
             final int halfWidth = width / 2;
 
-            // Calculate the largest inSampleSize value that is a power of 2 and keeps both
-            // height and width larger than the requested height and width.
+            // Find the largest power of 2 that maintains height AND width larger than the
+            // required width and height
             while ((halfHeight / inSampleSize) >= reqHeight
                     && (halfWidth / inSampleSize) >= reqWidth) {
                 inSampleSize *= 2;
