d3f6f6da5831894899ddce1d7293c6be8cf34cad
Philip Woods <sc14pw@leeds.ac.uk>
2017-06-18 18:25:22 +0100

    Modified widget cycling approach:     - Now uses a shared preference value to track the image       index of the currently displayed image on the widget     - This is over the use of a static variable

--- app/src/main/java/base/earthgrabber/WidgetProvider.java
+++ app/src/main/java/base/earthgrabber/WidgetProvider.java
@@ -6,9 +6,11 @@ import android.appwidget.AppWidgetProvider;
 import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.content.res.Resources;
 import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.util.Log;
 import android.widget.RemoteViews;
 
@@ -22,7 +24,7 @@ public class WidgetProvider extends AppWidgetProvider {
 
 
     private static final String SYNC_CLICKED    = "base.earthgrabber.WidgetClick";
-    static int imageCycle = 0;
+    int imageCycle;
 
 
     @Override
@@ -37,6 +39,10 @@ public class WidgetProvider extends AppWidgetProvider {
         // Widget size currently set to 250 x 110dp
 
         for (int i = 0; i < count; i++) {
+
+            SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(context);
+            imageCycle = preferences.getInt("Name", 0);
+
             // Get the file path and load the bitmap
             String filePath = context.getFilesDir().listFiles()[imageCycle].getPath();
             Bitmap bitmap = bmLoader.getScaledBitmapFromFile(filePath, displayWidth, displayHeight);
@@ -55,7 +61,10 @@ public class WidgetProvider extends AppWidgetProvider {
 
             //Finally notify the manager to update the widget
             appWidgetManager.updateAppWidget(widgetId, remoteViews);
-            imageCycle = (imageCycle + 1) % context.getFilesDir().list().length;
+
+            SharedPreferences.Editor editor = preferences.edit();
+            editor.putInt("imageCycle",(imageCycle + 1) % context.getFilesDir().list().length);
+            editor.apply();
         }
     }
 
@@ -71,6 +80,10 @@ public class WidgetProvider extends AppWidgetProvider {
                 int displayWidth = (int) (250 * scale + 0.5f);
                 int displayHeight = (int) (110 * scale + 0.5f);
 
+
+                SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(context);
+                imageCycle = preferences.getInt("imageCycle", 0);
+
                 String filePath = context.getFilesDir().listFiles()[imageCycle].getPath();
                 Bitmap bitmap = bmLoader.getScaledBitmapFromFile(filePath, displayWidth, displayHeight);
 
@@ -78,7 +91,10 @@ public class WidgetProvider extends AppWidgetProvider {
                         R.layout.widget_main);
                 remoteViews.setImageViewBitmap(R.id.imageView, bitmap);
                 AppWidgetManager.getInstance(context).updateAppWidget(widgetId, remoteViews);
-                imageCycle = (imageCycle + 1) % context.getFilesDir().list().length;
+
+                SharedPreferences.Editor editor = preferences.edit();
+                editor.putInt("imageCycle",(imageCycle + 1) % context.getFilesDir().list().length);
+                editor.apply();
 
             }
         }
